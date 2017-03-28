339f711c982b5ee177ee0b8df1d3b796cb7d4aea
wangmeng <1050202945@qq.com>
2017-03-28 19:54:59 +0800

    修改

--- src/main/java/com/cz/mts/system/schema/CardSchema.java
+++ src/main/java/com/cz/mts/system/schema/CardSchema.java
@@ -1,13 +1,16 @@
 package com.cz.mts.system.schema;
 
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang.time.DateFormatUtils;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import com.cz.mts.frame.common.BaseLogger;
+import com.cz.mts.frame.util.DateUtils;
 import com.cz.mts.frame.util.Finder;
 import com.cz.mts.system.entity.AppUser;
 import com.cz.mts.system.entity.Card;
@@ -82,6 +85,34 @@ public class CardSchema extends BaseLogger{
 					//给用户发推送
 					notificationService.notify(12, appUser.getId(), appUser.getId(), appUser.getCurrentLqNum()+"");
 				}
+			}
+		}
+		
+	}
+	
+	@Scheduled(cron="0 */10 0 * * ?")
+	public void updateSyNum() throws Exception{
+		logger.info("*****************剩余数量更新提醒******************");
+		
+		Finder finder = Finder.getSelectFinder(Card.class).append(" where 1=1 and status = 2 ");
+		List<Card> cards = cardService.queryForList(finder,Card.class);
+		if(null != cards && cards.size() > 0){
+			for (Card card : cards) {
+
+				Finder finderUser=Finder.getSelectFinder(UserCard.class).append(" where status=0 and cardId="+card.getId());
+				List<UserCard> userCards=userCardService.queryForList(finder,UserCard.class);
+				
+				int num=0;
+				
+				for (UserCard userCard : userCards) {
+					userCardService.deleteById(userCard.getId(), UserCard.class);
+					num += 1;
+				}
+				
+				card.setNum(num);
+				
+				cardService.update(card,true);
+				
 			}
 		}
 		
