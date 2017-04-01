a914dda4d58913000dad8272a27772548e9b0be0
www_jing_net <944731211@qq.com>
2017-04-01 17:27:33 +0800

    bug修改

--- src/main/java/com/cz/mts/system/service/impl/CardServiceImpl.java
+++ src/main/java/com/cz/mts/system/service/impl/CardServiceImpl.java
@@ -122,7 +122,7 @@ public class CardServiceImpl extends BaseSpringrainServiceImpl implements ICardS
 					if(2 == card.getStatus()){
 						finder.append(" and `status`=2 and num !=0");
 					}else if(4 == card.getStatus()){
-						finder.append(" and (`status`=2 and num=0) OR `status`=4  ");
+						finder.append(" and ((`status`=2 and num=0) OR `status`=4 ) ");
 					}else{
 						finder.append(" and status=:status");
 						finder.setParam("status", card.getStatus());
