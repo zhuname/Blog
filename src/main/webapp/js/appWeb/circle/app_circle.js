4eadaa20887c15455a646a3c9b5a49f089c7365c
wangmeng <1050202945@qq.com>
2017-10-28 16:55:11 +0800

    修改

--- src/main/webapp/js/appWeb/circle/app_circle.js
+++ src/main/webapp/js/appWeb/circle/app_circle.js
@@ -344,7 +344,6 @@ window.onscroll=function(){
 	var b = document.documentElement.scrollTop==0? document.body.scrollTop : document.documentElement.scrollTop;
 	var c = document.documentElement.scrollTop==0? document.body.scrollHeight : document.documentElement.scrollHeight;
 	if(a+b==c){
-		alert(11)
 		nextPage=nextPage+1;
 		show();
 	}
