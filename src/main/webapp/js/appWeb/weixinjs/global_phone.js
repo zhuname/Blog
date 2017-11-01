8e74968bc15675e8f18cffa9c98d08adc278540d
mts <1050202945@qq.com>
2017-11-01 20:51:18 +0800

    xiugai

--- src/main/webapp/js/appWeb/weixinjs/global_phone.js
+++ src/main/webapp/js/appWeb/weixinjs/global_phone.js
@@ -44,6 +44,7 @@ function GetDateDiff(diffTime) {
 
 function hrefIndexShare() {
 	alert(getQueryString("isShare"));
+	alert(getQueryString("state"));
 	if(getQueryString("isShare")!=undefined){
 		window.location.href="/mts/appWeb/index/index.jsp";
 	}else{
