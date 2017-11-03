b9868970738da5a0ec0aba5f12e8b18caeae6b55
mts <1050202945@qq.com>
2017-11-03 16:35:17 +0800

    xiugai

--- src/main/webapp/js/appWeb/weixinjs/global_phone.js
+++ src/main/webapp/js/appWeb/weixinjs/global_phone.js
@@ -71,7 +71,6 @@ $(function(){
 function hrefIndexShare(type) {
 	/*var test = document.referrer;
 	if(test+""==""){*/
-	
 	switch (type) {
 	case 1:
 		window.location.href="/mts/appWeb/posterPackage/posterPackage.jsp?cityId="+getCookie("htmlCityId");;
@@ -85,15 +84,12 @@ function hrefIndexShare(type) {
 	case 4:
 		window.location.href="/mts/appWeb/circle/circle.jsp?cityId="+getCookie("htmlCityId");;
 		break;
-	case 5:
+	default:
 		if(document.referrer!=""){
 			window.location.href=document.referrer;
 		}else{
 			window.location.href="/mts/appWeb/index/index.jsp";
 		}
-		break;
-	default:
-		window.location.href="/mts/appWeb/index/index.jsp";
 		break;
 	}
 	/*	return;
